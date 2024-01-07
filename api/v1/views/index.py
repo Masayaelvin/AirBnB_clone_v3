103823f51c557fb3e4c77f9402e3b6d7240e633d
Masaya Elvin <masayaelvin@gmail.com>
2024-01-07 15:26:36 +0300

    q4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,20 @@ def status():
     """creates a route status"""
 
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", methods=['GET'])
+def stats():
+    """defines the function to get the stats"""
+
+    amenities = storage.count("Amenities")
+    states = storage.count("States")
+    stats = {
+            "amenities": amenities,
+            "cities": 36,
+            "places": 154,
+            "reviews": 718,
+            "states": states,
+            "users": 31
+            }
+
+    return jsonify(stats)
