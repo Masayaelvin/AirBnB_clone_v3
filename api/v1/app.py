b2a5ef692a85eaff6e51c87e20b57f35e3c4b989
Masaya Elvin <masayaelvin@gmail.com>
2024-01-09 15:00:20 +0300

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-"""This module contains variables and methods used to connect to API"""
-
-from flask import Flask, Blueprint, jsonify
-from api.v1.views import app_views
-from models import storage
+"""Python api built with flask"""
+from flask import Flask, jsonify, make_response
 from flask_cors import CORS
-
+from models import storage
+from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
-cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
-host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = int(os.getenv('HBNB_API_PORT', '5000'))
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.url_map.strict_slashes = False
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_app(code):
-    '''
-        Handles teardown
-    '''
+def tear_down(exception):
+    '''Calls storage close on appcontext'''
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    '''
-        Returns a JSON-formatted error response
-    '''
-    return jsonify({"error": "Not found"}), 404
+def error_handler(error):
+    '''Returns a JSON formatted 404 status code'''
+    return jsonify({'error': 'Not found'}), 404
+
+
+@app.errorhandler(400)
+def error_handler2(error):
+    '''Returns a JSON formatted 404 status code'''
+    return make_response(jsonify({'error': 'Not a JSON'}), 400)
 
 
 if __name__ == "__main__":
